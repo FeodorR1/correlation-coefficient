496c980782f2f618c83eb4f95979b745f03686f0
FeodorR1 <120783771+FeodorR1@users.noreply.github.com>
2024-11-15 17:16:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def generate_list(size: int, averageX: int, deltaX: int, averageY: int, deltaY: 
 
     y_from_list: float = 0
     x_from_list: float = 0
+
     for i in random_values:
         x_from_list += i[0]
         y_from_list += i[1]
@@ -36,18 +37,33 @@ def correlation_coefficient(values: list[tuple[int]], averageX: float, averageY:
     standart_diviation_y = math.sqrt(sum_of_y / len(values))
 
     corcoe = corcoe / (standart_diviation_x * standart_diviation_y)
-    return corcoe
+    return standart_diviation_x, standart_diviation_y, corcoe, averageX, averageY
+
+def lin_regress(standart_diviation_x, standart_diviation_y, corcoe, averageX, averageY):
+    b1 = (standart_diviation_y / standart_diviation_x) * corcoe
+    b0 = averageY - averageX * b1
+    return b1, b0, averageX, averageY
 
 
 generated = generate_list(100, 100, 19, 33, 5)
 test = generated[0]
-
 x_values, y_values = zip(*test)
 
+b1, b0, averageX, averageY = lin_regress(*(correlation_coef:=correlation_coefficient(*generated)))
+x = x_values
+y = [(b0 + i * b1) for i in x]
+
+plt.plot(x, y, label='Regression line', color='blue')
 plt.scatter(x_values, y_values, color='blue', marker='o')
 plt.xlabel('X axis')
 plt.ylabel('Y axis')
 plt.title('Plot of Coordinates')
+
+# plt.plot(x_values, y_values)
+print("correlation coefficient: ", correlation_coef[2], '\n')
+print("intercept: ", b0, "slope: ", b1)
+
+plt.show()
 
 # plt.plot(x_values, y_values)
 print(correlation_coefficient(*generated))
